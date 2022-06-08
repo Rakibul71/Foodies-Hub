ab668a5a3e6ae74f146b25393c553dc18d617274
Rakibul71 <rakibulislam202019@gmail.com>
2022-06-09 00:46:59 +0600

    set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar container">
+            <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+            <div className="nav-links">
+                <a href="">Home</a>
+                <a href="">Recipes</a>
+                <a href="">Setting</a>
+            </div>
+        </div>
+        // <div className="navbar">
+        //     <p>rakib</p>
+        //     <p>rakib</p>
+        // </div>
+
+    )
+}
+
+export default Navbar
