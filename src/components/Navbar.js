9f0c68779783f85faa85b5d3dc09eeaf2e48be9e
Rakibul71 <rakibulislam202019@gmail.com>
2022-06-10 12:53:45 +0600

    complete sidebar and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,56 @@
 import React, { useState } from 'react'
+import Sidebar from './Sidebar';
+// import { Link, useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
+
+import { faHome, faList, faCog } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
     const [showSidebar, setShowSidebar] = useState(false);
+    // const location = useLocation()
+    const links = [
+        {
+            name: "Home",
+            path: "/",
+            icon: faHome
+        },
+        {
+            name: "Recipes",
+            path: "/recipes",
+            icon: faList
+        },
+        {
+            name: "Settings",
+            path: "/settings",
+            icon: faCog
+        }
+    ]
+    function closeSidebar() {
+        setShowSidebar(false)
+    }
     return (
-        <div className="navbar container">
-            <a href="#!" className="logo">F<span>oo</span>diesHub</a>
-            <div className="nav-links">
-                <a href="">Home</a>
-                <a href="">Recipes</a>
-                <a href="">Setting</a>
+        <>
+            <div className="navbar container">
+                <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+                <div className="nav-links">
+                    {links.map(link => (
+                        // <Link className={location.pathname === link.path ? "active" : ""} to={link.path} key={link.name}>{link.name}</Link>
+                        <a href={link.path} key={link.name}>{link.name}</a>
+                    ))}
+                    {/* <a href="">Home</a>
+                    <a href="">Recipes</a>
+                    <a href="">Setting</a> */}
+                </div>
+                <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
             </div>
-            <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-            </div>
-        </div>
+            {showSidebar && <Sidebar close={closeSidebar} links={links} />}
+            {/* {showSidebar && <Sidebar close={closeSidebar} />} */}
+            {/* <Sidebar links={links} /> */}
+        </>
 
     )
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,34 @@
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import { Link, useLocation } from "react-router-dom"
+
+// export default function Sidebar({ links, close }) {
+//     const location = useLocation()
+//     return (
+//         <div className="sidebar" onClick={close}>
+//             {links.map(link => (
+//                 <Link to={link.path} className={location.pathname === link.path ? "sidebar-link active" : "sidebar-link"} key={link.name}>
+//                     <FontAwesomeIcon icon={link.icon} />
+//                     {link.name}
+//                 </Link>
+//             ))}
+//         </div>
+//     )
+// }
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const Sidebar = ({ links, close }) => {
+    return (
+        <div className="sidebar " onClick={close}>
+            {links.map((link) => (
+                <a className="sidebar-link" href="#" key={link.name}>
+                    <FontAwesomeIcon icon={link.icon} />
+                    {link.name}
+                </a>
+
+            ))}
+        </div>
+    )
+}
+
+export default Sidebar
