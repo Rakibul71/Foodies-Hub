740f8d231a2f7abba2bee1bd3589240066335c9d
Rakibul71 <rakibulislam202019@gmail.com>
2022-06-11 11:56:16 +0600

    working on animation and try to solve the error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import "styles/index.scss"
 
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container main">
+        <HeroSection />
+      </div>
     </div>
   );
 }
